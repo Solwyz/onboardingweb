4e1267d7870ab393165204f8675cc852ef6641ae
VineeshSolwyz <vineesh.web.solwyz@gmail.com>
2024-10-07 10:12:01 +0530

    Font config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+
+  theme: { 
+   
+    extend: {
+      fontFamily: {
+        AnekLatin: ['Anek Latin','sans-serif'],
+      },
+      colors:{
+       'gray-textColor': '#AEAEAE'
+      }
+    },
   },
   plugins: [],
 }
