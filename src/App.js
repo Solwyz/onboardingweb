c6c9062a947e456836fa761e6b8ca1a547a99ff2
Ruthe46 <ruthinlosh.web.solwyz@gmail.com>
2024-10-07 10:48:51 +0530

    structure for login section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-import LogedInSection from './LogedInSection/LogedInSection';
+import LogedInSection from './Pages/LogedInSection/LogedInSection';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import LoginPage from './Pages/LoginPage/LoginPage';
+import SignUpPage from './Pages/SignUpPage/SignUpPage';
 
 function App() {
   return (
     <div className="App">
-  <LogedInSection/>
+      <Router>
+        <Routes>
+          <Route path='/' exact Component={LogedInSection} />
+          <Route path='/login' Component={LoginPage} />
+          <Route path='/signup' Component={SignUpPage} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Pages/LoginPage/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function LoginPage() {
+  return (
+    <div>
+      Login page
+    </div>
+  )
+}
+
+export default LoginPage
+
