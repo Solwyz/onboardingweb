a0bc7b784e0ae2aa9662397d1ec8ddf3736c07c5
VineeshSolwyz <vineesh.web.solwyz@gmail.com>
2024-10-09 14:14:58 +0530

    commit

--- src/Components/DashboardComponents/EmployeeInformation/EmployeeInformation.js
+++ src/Components/DashboardComponents/EmployeeInformation/EmployeeInformation.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
+import EmployeeInformationDetailed from '../EmployeeInformationDetailed/EmployeeInformationDetailed';
 
 function EmployeeInformation() {
+  const [showWhitePage, setShowWhitePage] = useState(false);
+
+  const handleAddEmployeeClick = () => {
+    setShowWhitePage(true);
+  };
+
   return (
-    <div>EmployeeInformation</div>
-  )
+    <div className='h-full w-full'>
+      {!showWhitePage ? (
+        <button
+          className='border p-3 rounded-lg bg-blue-900 text-white font-medium hover:bg-blue-700 duration-1000 flex justify-end'
+          onClick={handleAddEmployeeClick}
+        >
+          + Add Employee
+        </button>
+      ) : (
+        <EmployeeInformationDetailed />
+      )}
+    </div>
+  );
 }
 
-export default EmployeeInformation
+export default EmployeeInformation;
