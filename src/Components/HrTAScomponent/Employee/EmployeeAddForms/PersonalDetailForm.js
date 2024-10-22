10f87a50f18e7f2a4c90d2d7ac1b1d9553a6a8cd
Ruthe46 <ruthinlosh.web.solwyz@gmail.com>
2024-10-22 15:54:39 +0530

    personal details form added

--- src/Components/HrTAScomponent/Employee/EmployeeAddForms/PersonalDetailForm.js
+++ src/Components/HrTAScomponent/Employee/EmployeeAddForms/PersonalDetailForm.js
@@ -3,6 +3,7 @@ import tickIcon from '../../../../Assets/HrTas/check.svg';
 import DropDownArrow from "../../../../Assets/HrTas/drop-down-arrow.svg";
 
 function PersonalDetailForm() {
+    const [showContactForm, setShowContactForm] = useState(false)
     const [maritalStatus, setMaritalStatus] = useState('single'); // To manage marital status
     const [fatherName, setFatherName] = useState('');
     const [spouseName, setSpouseName] = useState('');
@@ -40,12 +41,88 @@ function PersonalDetailForm() {
         if (isFormValid) {
             // Perform action on form submit
             console.log('Form submitted');
+            setShowContactForm(true)
         }
     };
 
     return (
+        <div>
+        {!showContactForm ? 
         <div className='bg-[#F8FAFB] pl-6 pr-12'>
-            {/* Progress bar (omitted for brevity) */}
+            <div className='flex justify-center mt-[32px]'>
+                <ol className="flex items-center w-[678px]">
+                    {/* Step 1 */}
+                    <li className="flex flex-col items-start w-full text-center">
+                        <div className="flex w-full items-center text-[#2B2342] after:content-[''] after:w-full after:h-1 after:border-b-[1px] after:border-[#2B2342] after:inline-block">
+                            <span className="flex items-center justify-center w-5 h-5 bg-[#2B2342] rounded-full shrink-0">
+                                <img src={tickIcon} alt="" className="w-3 h-3" />
+                            </span>
+                        </div>
+                        <span className="mt-2 text-sm font-normal text-[#2B2342] ml-[-5px]">Basic</span>
+                    </li>
+
+                    {/* Step 2 */}
+                    <li className="flex flex-col items-start w-full text-center">
+                        <div className="flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b-[1px] after:border-[#2B2342] after:inline-block">
+                            <span className="flex items-center justify-center w-5 h-5 bg-[#2B2342] rounded-full shrink-0">
+                                <img src={tickIcon} alt="" className="w-3 h-3" />
+                            </span>
+                        </div>
+                        <span className="mt-2 text-sm font-normal text-[#2B2342] ml-[-20px]">Professional</span>
+                    </li>
+
+                    {/* Step 3 */}
+                    <li className="flex flex-col items-start w-full text-center">
+                        <div className="flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b-[1px] after:border-[#2B2342] after:inline-block">
+                            <span className="flex items-center justify-center w-5 h-5 bg-[#2B2342] rounded-full shrink-0">
+                                <img src={tickIcon} alt="" className="w-3 h-3" />
+                            </span>
+                        </div>
+                        <span className="mt-2 text-sm font-normal text-[#696A70] ml-[-5px]">Salary</span>
+                    </li>
+
+                    {/* Step 4 */}
+                    <li className="flex flex-col items-start w-full text-center">
+                        <div className="flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b-[1px] after:border-[#C2C2C2] after:inline-block">
+                            <span className="flex items-center justify-center w-5 h-5 bg-[#2B2342] rounded-full shrink-0">
+                                <img src={tickIcon} alt="" className="w-3 h-3" />
+                            </span>
+                        </div>
+                        <span className="mt-2 text-sm font-normal text-[#696A70] ml-[-5px]">Personal</span>
+                    </li>
+
+                    {/* Step 5 */}
+                    <li className="flex flex-col items-start w-full text-center">
+                        <div className="flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b-[1px] after:border-[#C2C2C2] after:inline-block">
+                            <span className="flex items-center justify-center w-5 h-5 bg-[#DCDCDC] rounded-full shrink-0">
+                                <img src={tickIcon} alt="" className="w-3 h-3" />
+                            </span>
+                        </div>
+                        <span className="mt-2 text-sm font-normal text-[#696A70] ml-[-5px]">Contact</span>
+                    </li>
+
+                    {/* Step 6 */}
+                    <li className="flex flex-col items-start w-full text-center">
+                        <div className="flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b-[1px] after:border-[#C2C2C2] after:inline-block">
+                            <span className="flex items-center justify-center w-5 h-5 bg-[#DCDCDC] rounded-full shrink-0">
+                                <img src={tickIcon} alt="" className="w-3 h-3" />
+                            </span>
+                        </div>
+                        <span className="mt-2 text-sm font-normal text-[#696A70] ml-[-5px]">Physical</span>
+                    </li>
+
+                    {/* Final Step */}
+                    <li className="flex flex-col items-start w-full text-center">
+                        <div className="flex items-center w-full">
+                            <span className="flex items-center justify-center w-5 h-5 bg-[#DCDCDC] rounded-full shrink-0">
+                                {/* No tick icon for this step */}
+                            </span>
+                        </div>
+                        <span className="mt-2 text-sm font-normal text-[#696A70]">Final</span>
+                    </li>
+                </ol>
+            </div>
+
 
             <div className='Details-form bg-white mt-8 p-6'>
                 <div>
@@ -56,7 +133,7 @@ function PersonalDetailForm() {
                     <div className='flex mt-[16px]'>
                         <div>
                             <div className="block text-sm text-[#373737] font-normal">Marital Status</div>
-                            <div 
+                            <div
                                 className="w-[247px] h-[48px] px-[16px] py-[14px] mt-[8px] border rounded-[8px] border-[#E6E6E7] text-[14px] text-[#696A70] font-normal flex items-center justify-between cursor-pointer"
                                 onClick={() => setShowDropdown(!showDropdown)}
                             >
@@ -95,9 +172,8 @@ function PersonalDetailForm() {
                                     setFatherName(e.target.value);
                                     handleFormValidation();
                                 }}
-                                className={`w-[247px] focus:outline-none h-[48px] px-[16px] py-[14px] mt-[8px] border rounded-[8px] border-[#E6E6E7] text-[14px] font-normal ${
-                                    maritalStatus === 'married' ? 'bg-gray-100 text-gray-400 cursor-not-allowed' : 'text-[#696A70]'
-                                }`}
+                                className={`w-[247px] focus:outline-none h-[48px] px-[16px] py-[14px] mt-[8px] border rounded-[8px] border-[#E6E6E7] text-[14px] font-normal ${maritalStatus === 'married' ? 'bg-gray-100 text-gray-400 cursor-not-allowed' : 'text-[#696A70]'
+                                    }`}
                                 disabled={maritalStatus === 'married'}
                             />
                         </div>
@@ -115,9 +191,8 @@ function PersonalDetailForm() {
                                     setSpouseName(e.target.value);
                                     handleFormValidation();
                                 }}
-                                className={`w-[247px] focus:outline-none h-[48px] px-[16px] py-[14px] mt-[8px] border rounded-[8px] border-[#E6E6E7] text-[14px] font-normal ${
-                                    maritalStatus === 'single' ? 'bg-gray-100 text-gray-400 cursor-not-allowed' : 'text-[#696A70]'
-                                }`}
+                                className={`w-[247px] focus:outline-none h-[48px] px-[16px] py-[14px] mt-[8px] border rounded-[8px] border-[#E6E6E7] text-[14px] font-normal ${maritalStatus === 'single' ? 'bg-gray-100 text-gray-400 cursor-not-allowed' : 'text-[#696A70]'
+                                    }`}
                                 disabled={maritalStatus === 'single'}
                             />
                         </div>
@@ -137,6 +212,8 @@ function PersonalDetailForm() {
                     </button>
                 </div>
             </div>
+        </div> :
+        <div>Contact form here</div> }
         </div>
     );
 }
